16fba69e1d9779c93b997236ea5d09898e9e4f64
Steve Goldman <gutezach@gmail.com>
2018-04-12 22:03:09 -0400

    Added Person Class

---
+++ Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenge
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string Age { get; set; }
+
+        public string City { get; set; }
+
+        public string Gender { get; set; }
+
+        public string State { get; set; }
+
+        public string IsStudent { get; set; }
+
+        public string IsEmployee { get; set; }
+    }
+}
